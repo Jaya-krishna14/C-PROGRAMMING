b051ed30a84e8fb7028238c53f82fd64f1f0107f
Jaya-krishna14 <jayakrishnasetti@gmail.com>
2025-12-03 20:17:24 +0530

    Add files via upload

---
+++ teana.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+   int a,b,c;
+   printf("entera");
+   scanf("%d",&a);
+   printf("enterb");
+   scanf("%d",&b);
+   c=a+b;
+   printf("addition is%d",c);
+"return0";   
+}
